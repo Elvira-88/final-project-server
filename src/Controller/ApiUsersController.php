465ba5c69f74a46e4e2593475c7835c933a57f67
elvira <elvipt88@gmail.com>
2021-07-01 14:27:24 +0200

    Register in Backend

--- src/Controller/ApiUsersController.php
+++ src/Controller/ApiUsersController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\TeachersRepository;
 use App\Repository\UserRepository;
 use App\Service\CourseNormalize;
 use App\Service\EnrollmentNormalize;
 use App\Service\TeacherNormalize;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/api/users", name="api_users_")
@@ -38,5 +43,68 @@ class ApiUsersController extends AbstractController
 
         return $this->json($data);
     }    
+
+
+    /**
+     * @Route(
+     *      "",
+     *      name="post",
+     *      methods={"POST"}
+     * )
+     */
+    public function add(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,     
+        UserPasswordHasherInterface $hasher
+        ): Response {
+        $data = json_decode($request->getContent());
+        $data->name;
+        dump($data);
+        die();
+                 
+        $user = new User();
+
+        $user->setName($data->name);
+        $user->setLastName($data->get('lastname'));
+        $user->setDni($data->get('dni'));
+        $user->setPhone($data->get('phone'));
+        $user->setAddress($data->get('adress'));
+        $user->setEmail($data->get('email'));
+
+        $hash = $hasher->hashPassword($user, $data->password);
+        $user->setPassword($hash);
+        //password?                   
+
+        $errors = $validator->validate($user);
+
+        if(count($errors) > 0) {
+            $dataErrors = [];
+
+            /** @var \Symfony\Component\Validator\ConstraintViolation $error */
+            foreach($errors as $error) {
+                $dataErrors[] = $error->getMessage();
+            }
+
+            return $this->json([
+                'status' => 'error',
+                'data' => [
+                    'errors' => $dataErrors
+                ],
+            ],
+            Response::HTTP_BAD_REQUEST);
+        }
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+       
+        return  $this->json(
+           
+            Response::HTTP_CREATED,        
+
+        );
+    }
     
 }
